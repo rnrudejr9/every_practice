79435afd74137191b0e923289fff59274007d22f
rnrudejr9 <rnrudejr1234@naver.com>
2023-07-25 23:58:09 +0900

    feat : 하이퍼링크 기능 추가 + blob 문제해결

--- script/update-readme.py
+++ script/update-readme.py
@@ -29,12 +29,14 @@ def print_files_in_dir(root_dir, prefix ,problems):
                 if ext == '.java':
                     print("%s %s" % (root, filename))
                     split_dir = root.split("/")
+                    address = root.replace("..","/tree/master")
+
                     if len(split_dir) >= 4:
                         week = str(split_dir[2])
                         day = str(split_dir[3])
                         filename = str(filename)
                         if week and day and filename:
-                            problems.append(Problem(str(value),week,day,filename,root))
+                            problems.append(Problem(str(value),week,day,filename,address))
                             value += 1
     except PermissionError:
         pass
