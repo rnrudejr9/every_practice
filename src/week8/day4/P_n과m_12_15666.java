77bdc3b61d367886cf9d3af5799e53cbf1e4ce7e
rnrudejr9 <rnrudejr1234@naver.com>
2023-08-10 21:14:55 +0900

    feat : week8 day4

---
+++ src/week8/day4/P_n과m_12_15666.java
@@ -0,0 +1,19 @@
+package week8.day4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P_n과m_12_15666 {
+
+    static int n;
+    static int m;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+    }
+}
