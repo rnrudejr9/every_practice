c69803cc47461f099217f700125e52ebe6121e24
rnrudejr9 <rnrudejr1234@naver.com>
2023-08-12 12:33:46 +0900

    feat : week8 day6

---
+++ src/week8/day6/P_고양이.java
@@ -0,0 +1,14 @@
+package week8.day6;
+
+public class P_고양이 {
+    public static void main(String[] args){
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("\\    /\\\n");
+        sb.append(" )  ( ')\n");
+        sb.append("(  /  )\n");
+        sb.append(" \\(__)|\n");
+
+        System.out.println(sb);
+    }
+}
