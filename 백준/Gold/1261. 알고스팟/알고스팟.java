d121b86cf4154b76a8ced0d21364f2032b903af7
koopi <49141751+rnrudejr9@users.noreply.github.com>
2023-10-12 10:27:48 +0900

    [Gold IV] Title: 알고스팟, Time: 152 ms, Memory: 14788 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,26 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-/**
- * 참고
- * 메모리초과 -> 방문처리 잘못했었음
- * 
- * @author SSAFY
- *
- */
+
 public class Main {
 	static int m, n;
 	static int[][] map;
 	static int[] dx = {1, 0, -1, 0};
 	static int[] dy = {0, 1, 0, -1};
 	static boolean[][] visited;
+	static int[][] distance;
 
-	static class Point {
+	static class Point implements Comparable<Point> {
 		int x;
 		int y;
 		public int cnt;
@@ -32,6 +26,11 @@ public class Main {
 			this.cnt = cnt;
 		}
 
+		@Override
+		public int compareTo(Point p) {
+			return Integer.compare(cnt, p.cnt);
+		}
+
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -41,47 +40,39 @@ public class Main {
 		n = Integer.parseInt(st.nextToken());
 		map = new int[n][m];
 		visited = new boolean[n][m];
+		distance = new int[n][m];
 		for (int i = 0; i < n; i++) {
 			String str = br.readLine();
 			for (int j = 0; j < m; j++) {
 				map[i][j] = str.charAt(j) - '0';
 			}
+			Arrays.fill(distance[i], Integer.MAX_VALUE);
 		}
-		int ans = BFS();
-		System.out.println(ans);
+		dijkstra();
+		System.out.println(distance[n-1][m-1]);
 	}
 
-	public static int BFS() {
-		PriorityQueue<Point> que = new PriorityQueue<>((e1, e2) -> Integer.compare(e1.cnt, e2.cnt));
-		que.offer(new Point(0, 0, 0));
-		visited[0][0] = true;
-		
-		while (!que.isEmpty()) {
-			Point p = que.poll();
-			int x = p.x;
-			int y = p.y;
-			int level = p.cnt;
-
-			if (x == n - 1 && y == m - 1) {
-				return level;
-			}
-
+	public static void dijkstra() {
+		PriorityQueue<Point> pq = new PriorityQueue<>();
+		distance[0][0] = 0;
+		pq.offer(new Point(0, 0, 0));
+		while (!pq.isEmpty()) {
+			Point cur = pq.poll();
 			for (int i = 0; i < dx.length; i++) {
-				int mx = x + dx[i];
-				int my = y + dy[i];
+				int mx = dx[i] + cur.x;
+				int my = dy[i] + cur.y;
+				int level = cur.cnt;
 				if (check(mx, my)) {
-					if (!visited[mx][my]) {
-						visited[mx][my] = true;
-						if (map[mx][my] == 1) {
-							que.offer(new Point(mx, my, level + 1));
-						} else {
-							que.offer(new Point(mx, my, level));
-						}
+					if(map[mx][my] == 1) {
+						level++;
+					}
+					if(distance[mx][my] > level) {
+						distance[mx][my] =level;
+						pq.offer(new Point(mx,my,level));
 					}
 				}
 			}
 		}
-		return -1;
 	}
 
 	private static boolean check(int mx, int my) {
